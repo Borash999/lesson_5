4e244fc91d538cf3e7e8dc38c22b1955892391d5
Vlad <vladik.bar4noff@yandex.ru>
2023-07-07 18:14:17 +0900

    lesson_5

--- 1.py
+++ 1.py
@@ -1,10 +1,7 @@
 # Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
 from typing import Generator
-
-abs_path = r'C:\Thecode\Media\статья.txt'
-
-
+abs_path = r'D:\Учеба\IT\ДЗ\Homeworks\Python\lesson_5\1.py'
 def split_path(abs_path: str) -> tuple():
     list_abs_path = abs_path.split('\\')
     list_last_elem = list_abs_path[-1].split('.')
@@ -12,6 +9,4 @@ def split_path(abs_path: str) -> tuple():
     name = list_last_elem[0]
     expansion = list_last_elem[1]
     return path, name, expansion
-
-
 print(split_path(abs_path))
